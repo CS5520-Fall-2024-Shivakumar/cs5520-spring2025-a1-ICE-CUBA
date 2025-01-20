191b0977db69234ab2b0f4ae79329b12be42145e
ICE-CUBA <fiberalfu@outlook.com>
2025-01-19 21:52:59 -0800

    A1-setting up

---
+++ app/src/main/java/com/example/numad25sp_linyanfu/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.numad25sp_linyanfu;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView t = findViewById(R.id.text1);
+        TextView t2 = findViewById(R.id.text2);
+
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(v -> {
+            t.setText("Linyan Fu");
+            t2.setText("fu.liny@northeastern.edu");
+        });
+
+    }
+}
